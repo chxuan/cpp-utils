387e9c5b2a4ae6dd2e33aeb250aca8d0fadaadc2
chxuan <787280310@qq.com>
2016-05-11 16:04:18 +0800

    Update BindCPU

--- BindCPU/BindCPU.cpp
+++ BindCPU/BindCPU.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+#include <unistd.h>
+#ifndef __USE_GNU
+#define __USE_GNU   // 为了使用SET_SET()等宏定义，但unistd.h里面好像已经定义了
+#endif
+#include <sched.h>
+#include <stdio.h>
+
+unsigned int systemCpuNum()
+{
+    return sysconf(_SC_NPROCESSORS_CONF);
+}
 
 int main()
 {
+    unsigned int cpuNum = systemCpuNum();
+    printf("Current system has %u CPU(s)\n", cpuNum);
+
+    cpu_set_t cpuSet;
+    CPU_ZERO(&cpuSet);
+
     return 0;
 }
