552e92d4a77e46338aefafd215091d35b976995b
chxuan <787280310@qq.com>
2016-05-24 22:04:16 +0800

    Update connect

---
+++ Connect/main.cpp
@@ -0,0 +1,59 @@
+#include "Connect.hpp"
+#include <iostream>
+#include <string>
+
+class A
+{
+public:
+    void start()
+    {
+        emit m_s1();
+        emit m_s2("Hello C++11");
+        emit m_s3(100, "Hello C++11");
+    }
+
+signals:
+    Signal<> m_s1;
+    Signal<std::string> m_s2;
+    Signal<int, std::string> m_s3;
+};
+
+class B
+{
+public slots:
+    void func1()
+    {
+        std::cout << "func1" << std::endl;
+    }
+
+    void func2(const std::string& str)
+    {
+        std::cout << str << std::endl;
+    }
+
+    void func3(int n, const std::string& str)
+    {
+        std::cout << n << " " << str << std::endl;
+    }
+};
+
+void func(const std::string& str)
+{
+    std::cout << "func " << str << std::endl;
+}
+
+int main()
+{
+    A a;
+    B b;
+
+    connect(&a, m_s1, std::bind(&B::func1, &b));
+    connect(&a, m_s2, std::bind(&B::func2, &b, std::placeholders::_1));
+    connect(&a, m_s2, std::bind(func, std::placeholders::_1));
+    connect(&a, m_s3, std::bind(&B::func3, &b, std::placeholders::_1, std::placeholders::_2));
+
+    a.start();
+
+    return 0;
+}
+
