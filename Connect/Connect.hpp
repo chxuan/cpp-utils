b5d4cf8b0d378d485e13027995ed3be18a76a2f8
chxuan <787280310@qq.com>
2016-05-24 22:00:46 +0800

    Update connect

--- Connect/Connect.hpp
+++ Connect/Connect.hpp
@@ -2,64 +2,58 @@
 #define _CONNECT_H
 
 #include <vector>
+#include <memory>
 #include <functional>
 
 #define emit
 #define slots
 #define signals public
-#define connect(sender, signal, receiver, slot) ((sender)->signal.bind(receiver, slot))
+#define connect(sender, signal, slot) ((sender)->signal.bind(slot))
 
-template<typename T1>
-class SlotBase
-{
-public:
-    virtual ~SlotBase() {}
-    virtual void exec(T1 t1) = 0;
-};
-
-template<typename T, typename T1>
-class SlotImpl : public SlotBase<T1>
-{
-public:
-    SlotImpl(T* receiver, void (T::*func)(T1))
-    {
-        m_receiver = receiver;
-        m_func = func;
-    }
-
-    virtual void exec(T1 t1)
-    {
-        (m_func->*m_func)(t1);
-    }
-
-private:
-    T* m_receiver = nullptr;
-    void (T::*m_func)(T1);
-};
-
-template<typename T1>
+template<typename... Args>
 class Slot
 {
 public:
-    template<typename T>
-    Slot(T* receiver, void (T::*func)(T1))
+    using OnFunc = std::function<void(Args...)>;
+
+    Slot(const OnFunc& func)
+        : m_func(func)
     {
-        m_slotBase = new SlotImpl<T, T1>(receiver, func); 
+        // Do nothing
     }
 
-    ~Slot()
+    void exec(Args... args)
     {
-        delete m_slotBase;
-        m_slotBase = NULL;
-    }
-
-    void exec(T1 t1)
-    {
-        m_slotBase->exec(t1);
+        m_func(args...);
     }
 
 private:
-    SlotBase<T1>* m_slotBase;
+    OnFunc m_func = nullptr;
+};
+
+template<typename... Args>
+class Signal
+{
+public:
+    using SlotPtr = std::shared_ptr<Slot<Args...>>; 
+    using OnFunc = std::function<void(Args...)>;
+
+    void bind(const OnFunc& func)
+    {
+        SlotPtr s(new Slot<Args...>(func));
+        m_slotVec.push_back(s);
+    }
+
+    void operator()(Args... args)
+    {
+        for (auto& iter : m_slotVec)
+        {
+            iter->exec(args...);
+        }
+    }
+
+private:
+    std::vector<SlotPtr> m_slotVec;
 };
 
 #endif
