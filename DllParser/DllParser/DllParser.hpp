ec96c6c8248ba4dc45c405f14db4c8eeb1341158
chxuan <787280310@qq.com>
2016-06-01 22:53:07 +0800

    Update dllparser

--- DllParser/DllParser/DllParser.hpp
+++ DllParser/DllParser/DllParser.hpp
@@ -1,3 +1,89 @@
 #ifndef _DLLPARSER_H
 #define _DLLPARSER_H
+
+#include <dlfcn.h>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <functional>
+#include <memory>
+
+class DllParser
+{
+public:
+    ~DllParser()
+    {
+        unload();
+    }
+
+    bool load(const std::string& dllFilePath)
+    {
+        m_handle = dlopen(dllFilePath.c_str(), RTLD_NOW);
+        if (m_handle == nullptr)
+        {
+            std::cout << "Open dll failed, error: " << dlerror() << std::endl;
+            return false;
+        }
+
+        return true;
+    }
+
+    bool unload()
+    {
+        if (m_handle == nullptr)
+        {
+            return true;
+        }
+
+        int ret = dlclose(m_handle);
+        m_handle = nullptr;
+        if (ret != 0)
+        {
+            std::cout << "Close dll failed, error: " << dlerror() << std::endl;
+            return false;
+        }
+
+        return true;
+    }
+
+    template<typename T>
+    std::function<T> getFunction(const std::string& funcName)
+    {
+        auto iter = m_funcMap.find(funcName);
+        if (iter == m_funcMap.end())
+        {
+            void* addr = dlsym(m_handle, funcName.c_str());
+            if (addr == nullptr)
+            {
+                std::cout << "func is nullptr, error: " << dlerror() << std::endl;
+                return nullptr;
+            }
+
+            m_funcMap.insert(std::make_pair(funcName, addr));
+            iter = m_funcMap.find(funcName);
+        }
+        
+        return std::function<T>(reinterpret_cast<T*>(iter->second));
+    }
+
+    template<typename T, typename... Args>
+    typename std::result_of<std::function<T>(Args...)>::type excecuteFunction(const std::string& funcName, Args&&... args)
+    {
+        auto func = getFunction<T>(funcName);
+        if (func == nullptr)
+        {
+            std::string str = "Can not find this function: " + funcName;
+            throw std::runtime_error(str);
+        }
+
+        return func(std::forward<Args>(args)...);
+    }
+
+private:
+    void* m_handle = nullptr;
+    std::unordered_map<std::string, void*> m_funcMap;
+};
+
+using DllParserPtr = std::shared_ptr<DllParser>;
+
 #endif
--- DllParser/DllParser/main.cpp
+++ DllParser/DllParser/main.cpp
@@ -1,44 +1,37 @@
-#include <dlfcn.h>
-#include <iostream>
-#include <string>
+#include "DllParser.hpp"
 
 int main()
 {
     std::string dllFilePath = "../Caculate/libcac.so";
-    void* handle = dlopen(dllFilePath.c_str(), RTLD_NOW);
-    if (handle == nullptr)
+
+    DllParserPtr dllParser(new DllParser);
+    bool ret = dllParser->load(dllFilePath);
+    if (!ret)
     {
-        std::cout << "Open dll: " << dllFilePath << " failed, error: " << dlerror() << std::endl;
         return 1;
     }
 
     int a = 20;
     int b = 10;
 
-    using OnFunc = int(*)(int, int);
-    OnFunc func = nullptr;
-
-    func = reinterpret_cast<OnFunc>(dlsym(handle, "add"));
-    if (func == nullptr)
+    try
     {
-        std::cout << "func is nullptr, error: " << dlerror() << std::endl;
-        return 1;
+        auto addRet = dllParser->excecuteFunction<int(int, int)>("add", a, b);
+        std::cout << "a + b = " << addRet << std::endl;
     }
-    std::cout << "a + b = " << func(a, b) << std::endl;
-
-    func = reinterpret_cast<OnFunc>(dlsym(handle, "sub"));
-    if (func == nullptr)
+    catch (std::exception& e)
     {
-        std::cout << "func is nullptr, error: " << dlerror() << std::endl;
-        return 1;
+        std::cout << "Exception: " << e.what() << std::endl;
     }
-    std::cout << "a - b = " << func(a, b) << std::endl;
 
-    int ret = dlclose(handle);
-    if (ret != 0)
+    try
     {
-        std::cout << "Close dll: " << dllFilePath << " failed, error: " << dlerror() << std::endl;
-        return 1;
+        auto subRet = dllParser->excecuteFunction<int(int, int)>("sub", a, b);
+        std::cout << "a - b = " << subRet << std::endl;
+    }
+    catch (std::exception& e)
+    {
+        std::cout << "Exception: " << e.what() << std::endl;
     }
 
     return 0;
