42125f8e76352b26c16f837efa1a46ccba3662ec
chxuan <787280310@qq.com>
2016-06-01 15:22:54 +0800

    Update dllparser

--- DllParser/DllParser/main.cpp
+++ DllParser/DllParser/main.cpp
@@ -1,7 +1,34 @@
+#include <dlfcn.h>
 #include <iostream>
+#include <string>
 
 int main()
 {
+    std::string dllFilePath = "libcac.so";
+    void* handle = dlopen(dllFilePath.c_str(), RTLD_NOW);
+    if (handle == nullptr)
+    {
+        std::cout << "Open dll: " << dllFilePath << " failed, error: " << dlerror() << std::endl;
+    }
+
+    int a = 20;
+    int b = 10;
+
+    using OnFunc = int(*)(int, int);
+    OnFunc func;
+
+    func = (OnFunc)dlsym(handle, "add");
+    std::cout << "a + b = " << func(a, b) << std::endl;
+
+    func = (OnFunc)dlsym(handle, "sub");
+    std::cout << "a - b = " << func(a, b) << std::endl;
+
+    int ret = dlclose(handle);
+    if (ret != 0)
+    {
+        std::cout << "Close dll: " << dllFilePath << " failed, error: " << dlerror() << std::endl;
+    }
+
     return 0;
 }
 
