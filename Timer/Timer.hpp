07892dd82fad536951d690a453bd10fc73e05cfc
chxuan <787280310@qq.com>
2016-06-10 10:06:00 +0800

    Update Timer

---
+++ Timer/Timer.hpp
@@ -0,0 +1,61 @@
+#ifndef _TIMER_H
+#define _TIMER_H
+
+#include <chrono>
+
+class Timer
+{
+public:
+    Timer()
+        : m_begin(std::chrono::high_resolution_clock::now())
+    {
+        // Do nothing
+    }
+
+    void reset()
+    {
+        m_begin = std::chrono::high_resolution_clock::now();
+    }
+
+    // 默认输出毫秒
+    template<typename Duration = std::chrono::milliseconds>
+    long long elapsed() const
+    {
+        return std::chrono::duration_cast<Duration>(std::chrono::high_resolution_clock::now() - m_begin).count();
+    }
+
+    // 微秒
+    long long elapsedMicroseconds() const
+    {
+        return elapsed<std::chrono::microseconds>();
+    }
+
+    // 纳秒
+    long long elapsedNanoSeconds() const
+    {
+        return elapsed<std::chrono::nanoseconds>();
+    }
+
+    // 秒
+    long long elapsedSeconds() const
+    {
+        return elapsed<std::chrono::seconds>();
+    }
+
+    // 分
+    long long elapsedMinutes() const
+    {
+        return elapsed<std::chrono::minutes>();
+    }
+
+    // 时
+    long long elapsedHours() const
+    {
+        return elapsed<std::chrono::hours>();
+    }
+
+private:
+    std::chrono::time_point<std::chrono::high_resolution_clock> m_begin;
+};
+
+#endif
---
+++ Timer/main.cpp
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <iostream>
+#include "Timer.hpp"
+
+int main()
+{
+    Timer t; // 开始计时
+
+    sleep(2);
+
+    std::cout << "milliseconds: " << t.elapsed() << std::endl;
+    std::cout << "microseconds: " << t.elapsedMicroseconds() << std::endl;
+    std::cout << "nanoseconds: " << t.elapsedNanoSeconds() << std::endl;
+    std::cout << "seconds: " << t.elapsedSeconds() << std::endl;
+    std::cout << "minutes: " << t.elapsedMinutes() << std::endl;
+    std::cout << "hours: " << t.elapsedHours() << std::endl;
+
+    return 0;
+}
