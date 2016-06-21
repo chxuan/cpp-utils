9c17c08268d36fde098a79ac8dade3b70c87fb3c
chxuan <787280310@qq.com>
2016-06-21 09:46:06 +0800

    Update base64

--- Base64/main.cpp
+++ Base64/main.cpp
@@ -2,9 +2,15 @@
  *该例程讲解base64接口的调用
 ************************************************/
 #include <iostream>
+#include <string>
 #include "Base64.hpp"
 
 int main()
 {
+    char buf[20] = "Hello world!";
+    std::string base64Str = base64_encode(buf, sizeof(buf)); 
+    std::cout << base64Str << std::endl;
+    std::string buf2 = base64_decode(base64Str);
+    std::cout << buf2 << std::endl;
     return 0;
 }
