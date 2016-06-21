258fe8c7beead0dd51f53395357d2b43892bc42c
chxuan <787280310@qq.com>
2016-06-21 09:52:06 +0800

    Update base64

--- Base64/Base64.hpp
+++ Base64/Base64.hpp
@@ -26,101 +26,105 @@ static const unsigned char un_b64_tab[] = {
 	255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255
 };
 
-static std::string base64_encode(const char *in, int len)
+class Base64
 {
-	const unsigned char *clear = (const unsigned char*)in;
+public:
+    static std::string encode(const char *in, int len)
+    {
+        const unsigned char *clear = (const unsigned char*)in;
 
-	std::string encoded;
-	encoded.resize(4 * ((len + 2) / 3));
-	auto p = &encoded[0];
+        std::string encoded;
+        encoded.resize(4 * ((len + 2) / 3));
+        auto p = &encoded[0];
 
-	while (len-- > 0)
-	{
-		int x, y;
+        while (len-- > 0)
+        {
+            int x, y;
 
-		x = *clear++;
-		*p++ = to_b64_tab[(x >> 2) & 63];
+            x = *clear++;
+            *p++ = to_b64_tab[(x >> 2) & 63];
 
-		if (len-- <= 0)
-		{
-			*p++ = to_b64_tab[(x << 4) & 63];
-			*p++ = '=';
-			*p++ = '=';
-			break;
-		}
+            if (len-- <= 0)
+            {
+                *p++ = to_b64_tab[(x << 4) & 63];
+                *p++ = '=';
+                *p++ = '=';
+                break;
+            }
 
-		y = *clear++;
-		*p++ = to_b64_tab[((x << 4) | ((y >> 4) & 15)) & 63];
+            y = *clear++;
+            *p++ = to_b64_tab[((x << 4) | ((y >> 4) & 15)) & 63];
 
-		if (len-- <= 0)
-		{
-			*p++ = to_b64_tab[(y << 2) & 63];
-			*p++ = '=';
-			break;
-		}
+            if (len-- <= 0)
+            {
+                *p++ = to_b64_tab[(y << 2) & 63];
+                *p++ = '=';
+                break;
+            }
 
-		x = *clear++;
-		*p++ = to_b64_tab[((y << 2) | ((x >> 6) & 3)) & 63];
+            x = *clear++;
+            *p++ = to_b64_tab[((y << 2) | ((x >> 6) & 3)) & 63];
 
-		*p++ = to_b64_tab[x & 63];
-	}
+            *p++ = to_b64_tab[x & 63];
+        }
 
-	encoded.resize(p - &encoded[0]);
-	return encoded;
-}
+        encoded.resize(p - &encoded[0]);
+        return encoded;
+    }
 
-static std::string base64_decode(const std::string& in)
-{
-	std::string decoded;
-	decoded.resize(3 * (in.size() / 4) + 1);
-	auto pd = &decoded[0];
+    static std::string decode(const std::string& in)
+    {
+        std::string decoded;
+        decoded.resize(3 * (in.size() / 4) + 1);
+        auto pd = &decoded[0];
 
-	/* Each cycle of the loop handles a quantum of 4 input bytes. For the last
-	quantum this may decode to 1, 2, or 3 output bytes. */
+        /* Each cycle of the loop handles a quantum of 4 input bytes. For the last
+           quantum this may decode to 1, 2, or 3 output bytes. */
 
-	auto pi = &in[0];
-	auto pend = pi + in.size();
-	while (pi != pend)
-	{
-		int x, y;
-		x = (*pi++);
+        auto pi = &in[0];
+        auto pend = pi + in.size();
+        while (pi != pend)
+        {
+            int x, y;
+            x = (*pi++);
 
-		if (x > 127 || (x = un_b64_tab[x]) == 255)
-			return "";
-		if (pi == pend)
-			return "";
-		y = (*pi++);
-		if ((y = un_b64_tab[y]) == 255)
-			return "";
-		*pd++ = (x << 2) | (y >> 4);
+            if (x > 127 || (x = un_b64_tab[x]) == 255)
+                return "";
+            if (pi == pend)
+                return "";
+            y = (*pi++);
+            if ((y = un_b64_tab[y]) == 255)
+                return "";
+            *pd++ = (x << 2) | (y >> 4);
 
-		if (pi != pend && ((x = (*pi++)) == '='))
-		{
-			if ((pi != pend && *pi++ != '=') || pi != pend)
-				return "";
-		}
-		else
-		{
-			if (x > 127 || (x = un_b64_tab[x]) == 255)
-				return "";
-			*pd++ = (y << 4) | (x >> 2);
+            if (pi != pend && ((x = (*pi++)) == '='))
+            {
+                if ((pi != pend && *pi++ != '=') || pi != pend)
+                    return "";
+            }
+            else
+            {
+                if (x > 127 || (x = un_b64_tab[x]) == 255)
+                    return "";
+                *pd++ = (y << 4) | (x >> 2);
 
-			if (pi != pend && ((y = (*pi++)) == '='))
-			{
-				if (pi != pend)
-					return "";
-			}
-			else
-			{
-				if (y > 127 || (y = un_b64_tab[y]) == 255)
-					return "";
-				*pd++ = (x << 6) | y;
-			}
-		}
-	}
+                if (pi != pend && ((y = (*pi++)) == '='))
+                {
+                    if (pi != pend)
+                        return "";
+                }
+                else
+                {
+                    if (y > 127 || (y = un_b64_tab[y]) == 255)
+                        return "";
+                    *pd++ = (x << 6) | y;
+                }
+            }
+        }
 
-	decoded.resize(pd - &decoded[0]);
-	return decoded;
-}
+        decoded.resize(pd - &decoded[0]);
+        return decoded;
+    }
+};
 
 #endif
--- Base64/main.cpp
+++ Base64/main.cpp
@@ -8,9 +8,9 @@
 int main()
 {
     char buf[20] = "Hello world!";
-    std::string base64Str = base64_encode(buf, sizeof(buf)); 
+    std::string base64Str = Base64::encode(buf, sizeof(buf)); 
     std::cout << base64Str << std::endl;
-    std::string buf2 = base64_decode(base64Str);
+    std::string buf2 = Base64::decode(base64Str);
     std::cout << buf2 << std::endl;
     return 0;
 }
