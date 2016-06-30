c4a25ad7781a45ac537664f2ed1bf173f8bb44d0
chxuan <787280310@qq.com>
2016-06-30 20:49:44 +0800

    Update variant

--- Variant/Variant.hpp
+++ Variant/Variant.hpp
@@ -1,3 +1,16 @@
 #ifndef _VARIANT_H
 #define _VARIANT_H
+
+#include <iostream>
+#include <typeindex>
+
+template<std::size_t arg, std::size_t... rest>
+struct IntergerMax;
+
+template<std::size_t arg>
+struct IntergerMax<arg> : std::integral_constant<std::size_t, arg> {};
+
+template<std::size_t arg1, std::size_t arg2, std::size_t... rest>
+struct IntergerMax<arg1, arg2, rest...> : std::integral_constant<std::size_t, arg1 >= arg2 ? IntergerMax<arg1, rest...>::value : IntergerMax<arg2, rest...>::value> {};
+
 #endif
