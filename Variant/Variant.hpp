c8fe4f6a213162f5960bf94dd7545225f34bd850
chxuan <787280310@qq.com>
2016-07-02 10:43:46 +0800

    Update variant

--- Variant/Variant.hpp
+++ Variant/Variant.hpp
@@ -28,5 +28,86 @@ struct Contains<T, Head, Rest...> : std::conditional<std::is_same<T, Head>::valu
 template<typename T>
 struct Contains<T> : std::false_type {};
 
+template<typename T, typename... List>
+struct IndexOf;
+
+template<typename T, typename Head, typename... Rest>
+struct IndexOf<T, Head, Rest...>
+{
+    enum
+    {
+        value = IndexOf<T, Rest...>::value + 1
+    };
+};
+
+template<typename T>
+struct IndexOf<T>
+{
+    enum
+    {
+        value = 0
+    };
+};
+
+template<int index, typename... Types>
+struct At;
+
+template<int index, typename First, typename... Types>
+struct At<index, First, Types...>
+{
+    using type = typename At<index - 1, Types...>::type;
+};
+
+template<typename T, typename... Types>
+struct At<0, T, Types...>
+{
+    using type = T;
+};
+
+template<typename... Types>
+class Variant
+{
+public:
+
+private:
+    void destory(const std::type_index& index, void* buf)
+    {
+        [](Types&&...){}((destory0<Types>(index, buf), 0)...);
+    }
+
+    template<typename T>
+    void destory0(const std::type_index& index, void* buf)
+    {
+        if (index == std::type_index(typeid(T)))
+        {
+            reinterpret_cast<T*>(buf)->~T();
+        }
+    }
+
+    void move(const std::type_index& oldIndex, void* oldValue, void* newValue)
+    {
+        [](Types&&...){}((move0<Types>(oldIndex, oldValue, newValue), 0)...);
+    }
+
+    template<typename T>
+    void move0(const std::type_index& oldIndex, void* oldValue, void* newValue)
+    {
+        if (oldIndex == std::type_index(typeid(T)))
+        {
+            new (newValue)T(std::move(*reinterpret_cast<T*>(oldValue)));
+        }
+    }
+
+private:
+    enum
+    {
+        dataSize = IntergerMax<sizeof(Types)...>::value,
+        alignSize = MaxAlign<Types...>::value
+    };
+    using data_t = typename std::aligned_storage<dataSize, alignSize>::type;
+    data_t m_data;
+    std::type_index m_typeIndex;
+};
+
 
 #endif
