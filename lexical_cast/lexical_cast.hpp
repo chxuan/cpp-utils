902e1d25a8ed9680b43c35ce3a560c7a7a831197
chxuan <787280310@qq.com>
2016-07-14 22:10:09 +0800

    Update lexical_cast

--- lexical_cast/lexical_cast.hpp
+++ lexical_cast/lexical_cast.hpp
@@ -12,6 +12,9 @@
 namespace detail
 {
 
+const char* strue = "true";
+const char* sfalse = "false";
+
 template<typename To, typename From>
 struct Converter {};
 
@@ -27,6 +30,82 @@ struct Converter<int, From>
     static int convert(const char* from)
     {
         return std::atoi(from);
+    }
+};
+
+template<typename From>
+struct Converter<long, From>
+{
+    static long convert(const std::string& from)
+    {
+        return std::atol(from.c_str());
+    }
+
+    static long convert(const char* from)
+    {
+        return std::atol(from);
+    }
+};
+
+template<typename From>
+struct Converter<long long, From>
+{
+    static long long convert(const std::string& from)
+    {
+        return std::atoll(from.c_str());
+    }
+
+    static long long convert(const char* from)
+    {
+        return std::atoll(from);
+    }
+};
+
+template<typename From>
+struct Converter<double, From>
+{
+    static double convert(const std::string& from)
+    {
+        return std::atof(from.c_str());
+    }
+
+    static double convert(const char* from)
+    {
+        return std::atof(from);
+    }
+};
+
+template<typename From>
+struct Converter<float, From>
+{
+    static float convert(const std::string& from)
+    {
+        return static_cast<float>(std::atof(from.c_str()));
+    }
+
+    static float convert(const char* from)
+    {
+        return static_cast<float>(std::atof(from));
+    }
+};
+
+// to bool
+template<typename From>
+struct Converter<bool, From>
+{
+    static typename std::enable_if<std::is_integral<From>::value, bool>::type convert(From from)
+    {
+        return from > 0 ? true : false;
+    }
+};
+
+// to string
+template<typename From>
+struct Converter<std::string, From>
+{
+    static std::string convert(const From& from)
+    {
+        return std::to_string(from);
     }
 };
 
--- lexical_cast/main.cpp
+++ lexical_cast/main.cpp
@@ -4,6 +4,10 @@
 int main()
 {
     std::string str = "1000";
+    int n = 200;
     std::cout << lexical_cast<int>(str) << std::endl;
+    std::cout << lexical_cast<std::string>(n) << std::endl;
+    std::cout << lexical_cast<bool>(n) << std::endl;
+    std::cout << lexical_cast<bool>(-1) << std::endl;
     return 0;
 }
