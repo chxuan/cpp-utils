1b86c686fb1430495f3360191056db90b2d76831
chxuan <787280310@qq.com>
2016-07-16 22:26:56 +0800

    Update FunctionTraits.hpp

--- FunctionTraits/FunctionTraits.hpp
+++ FunctionTraits/FunctionTraits.hpp
@@ -12,40 +12,40 @@ struct FunctionTraits;
 template<typename Ret, typename... Args>
 struct FunctionTraits<Ret(Args...)>
 {
-	enum 
+    enum 
     {
         arity = sizeof...(Args) 
     };
-	typedef Ret functionType(Args...);
+    typedef Ret functionType(Args...);
     using returnType = Ret;
-	using stlFunctionType = std::function<functionType>;
+    using stlFunctionType = std::function<functionType>;
     using pointer = Ret(*)(Args...);
 
 #if 0
     template<std::size_t N>
     using argType = typename std::tuple_element<N, std::tuple<Args...>>::type;
 #else
-	template<std::size_t N>
-	struct args
-	{
-		static_assert(N < arity, "Index is out of range, index must less than sizeof Args");
-		using type = typename std::tuple_element<N, std::tuple<Args...>>::type;
-	};
+    template<std::size_t N>
+    struct args
+    {
+	static_assert(N < arity, "Index is out of range, index must less than sizeof Args");
+	using type = typename std::tuple_element<N, std::tuple<Args...>>::type;
+    };
 #endif
 };
 
 // function pointer
 template<typename Ret, typename... Args>
-struct FunctionTraits<Ret(*)(Args...)> : public FunctionTraits<Ret(Args...)>{};
+struct FunctionTraits<Ret(*)(Args...)> : FunctionTraits<Ret(Args...)>{};
 
 // std::function
 template <typename Ret, typename... Args>
-struct FunctionTraits<std::function<Ret(Args...)>> : public FunctionTraits<Ret(Args...)>{};
+struct FunctionTraits<std::function<Ret(Args...)>> : FunctionTraits<Ret(Args...)>{};
 
 // member function
 #define MEMBER_FUNTION_TRAITS(...) \
 template <typename ReturnType, typename ClassType, typename... Args> \
-struct FunctionTraits<ReturnType(ClassType::*)(Args...) __VA_ARGS__> : public FunctionTraits<ReturnType(Args...)>{}; \
+struct FunctionTraits<ReturnType(ClassType::*)(Args...) __VA_ARGS__> : FunctionTraits<ReturnType(Args...)>{}; \
 
 MEMBER_FUNTION_TRAITS()
 MEMBER_FUNTION_TRAITS(const)
@@ -54,24 +54,24 @@ MEMBER_FUNTION_TRAITS(const volatile)
 
 // function object
 template<typename Callable>
-struct FunctionTraits : public FunctionTraits<decltype(&Callable::operator())>{};
+struct FunctionTraits : FunctionTraits<decltype(&Callable::operator())>{};
 
 template <typename Function>
 typename FunctionTraits<Function>::stlFunctionType toFunction(const Function& lambda)
 {
-	return static_cast<typename FunctionTraits<Function>::stlFunctionType>(lambda);
+    return static_cast<typename FunctionTraits<Function>::stlFunctionType>(lambda);
 }
 
 template <typename Function>
 typename FunctionTraits<Function>::stlFunctionType toFunction(Function&& lambda)
 {
-	return static_cast<typename FunctionTraits<Function>::stlFunctionType>(std::forward<Function>(lambda));
+    return static_cast<typename FunctionTraits<Function>::stlFunctionType>(std::forward<Function>(lambda));
 }
 
 template <typename Function>
 typename FunctionTraits<Function>::pointer toFunctionPointer(const Function& lambda)
 {
-	return static_cast<typename FunctionTraits<Function>::pointer>(lambda);
+    return static_cast<typename FunctionTraits<Function>::pointer>(lambda);
 }
 
 #endif
