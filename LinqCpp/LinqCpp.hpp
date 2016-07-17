69a17983bd2f18cf037df61caab0282862c14174
chxuan <787280310@qq.com>
2016-07-17 17:07:01 +0800

    Update linqcpp

--- LinqCpp/LinqCpp.hpp
+++ LinqCpp/LinqCpp.hpp
@@ -1,3 +1,90 @@
 #ifndef _LINQCPP_H
 #define _LINQCPP_H
+
+#include <type_traits>
+#include <numeric>
+#include <boost/algorithm/minmax_element.hpp>
+#include <boost/iterator/zip_iterator.hpp>
+#include <boost/range.hpp>
+#include <boost/range/adaptors.hpp>
+#include <boost/range/algorithm.hpp>
+#include <boost/range/join.hpp>
+
+namespace linqcpp
+{
+
+// 定义FunctionTraits用于将lambda表达式转换成function.
+template<typename Function>
+struct FunctionTraits : public FunctionTraits<decltype(&Function::operator())> {};
+
+template<typename ClassType, typename ReturnType, typename... Args>
+struct FunctionTraits<ReturnType(ClassType::*)(Args...) const>
+{
+    using function = std::function<ReturnType(Args...)>;
+};
+
+template<typename Function>
+typename FunctionTraits<Function>::function toFunction(const Function& lambda)
+{
+    return static_cast<typename FunctionTraits<Function>::function>(lambda);
+}
+
+template<typename R>
+class LinqCpp
+{
+public:
+    using valueType = typename R::value_type;
+    LinqCpp(R range) : m_linqRange(range) {}
+
+    // 过滤操作.
+    template<typename F>
+    auto where(const F& f)->LinqCpp<boost::filtered_range<typename FunctionTraits<F>::function, R>>
+    {
+        auto func = toFunction(f);
+        return LinqCpp<boost::filtered_range<typename FunctionTraits<F>::function, R>>(boost::adaptors::filter(m_linqRange, func));
+    }
+
+    // 转换操作.
+    template<typename F>
+    auto select(const F& f)->LinqCpp<boost::transformed_range<typename FunctionTraits<F>::function, R>>
+    {
+        auto func = toFunction(f);
+        return LinqCpp<boost::transformed_range<typename FunctionTraits<F>::function, R>>(boost::adaptors::transform(m_linqRange, func));
+    }
+
+    auto begin() const->decltype(std::begin(std::declval<const R>()))
+    {
+        return std::begin(m_linqRange);
+    }
+
+    auto end() const->decltype(std::end(std::declval<const R>()))
+    {
+        return std::end(m_linqRange);
+    }
+
+    auto max() const->valueType
+    {
+        return *std::max_element(begin(), end());
+    }
+
+private:
+    R m_linqRange;
+};
+
+};
+
+// 简化Range的声明
+template<template<typename T> class IteratorRange, typename R>
+using Range = IteratorRange<decltype(std::begin(std::declval<R>()))>;
+
+template<typename R>
+using iteratorRange = Range<boost::iterator_range, R>;
+
+// 简化定义LinqCpp的辅助函数
+template<typename R>
+linqcpp::LinqCpp<iteratorRange<R>> from(const R& range)
+{
+    return linqcpp::LinqCpp<iteratorRange<R>>(iteratorRange<R>(range));
+}
+
 #endif
--- LinqCpp/main.cpp
+++ LinqCpp/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include "LinqCpp.hpp"
 
 int main()
 {
+    std::vector<int> vec { 1, 2, 3, 4 };
+    auto range = from(vec).select([](int i){ return i + 2; });
+    for (auto iter : range)
+    {
+        std::cout << iter << " ";
+    }
+    std::cout << std::endl;
+
+    auto ret = from(vec).select([](int i){ return i + 2; }).where([](int i){ return i > 2; }).max();
+    std::cout << "max: " << ret << std::endl;
     return 0;
 }
