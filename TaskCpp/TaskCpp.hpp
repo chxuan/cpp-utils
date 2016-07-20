7a842a6f62dbb9b373160fef5511e559f681346c
chxuan <787280310@qq.com>
2016-07-20 14:33:31 +0800

    Update taskcpp

--- TaskCpp/TaskCpp.hpp
+++ TaskCpp/TaskCpp.hpp
@@ -1,17 +1,52 @@
 #ifndef _TASKCPP_H
 #define _TASKCPP_H
 
+#include <functional>
+#include <future>
+
 namespace taskcpp
 {
 
 template<typename T>
 class Task;
 
-template<typename R, typename... Args>
-class Task<R(Args...)>
+template<typename R, typename... ArgsType>
+class Task<R(ArgsType...)>
 {
 public:
+    Task(std::function<R(ArgsType...)>&& func) : m_func(std::move(func)) {}
+    Task(const std::function<R(ArgsType...)>& func) : m_func(func) {}
 
+    void wait()
+    {
+        std::async(m_func).wait();
+    }
+
+    template<typename... Args>
+    R get(Args&&... args)
+    {
+        return std::async(m_func, std::forward<Args>(args)...).get();
+    }
+
+    std::shared_future<R> run()
+    {
+        return std::async(m_func);
+    }
+
+    template<typename F>
+    auto then(F&& f)->Task<typename std::result_of<F(R)>::type(ArgsType...)>
+    {
+        using ReturnType = typename std::result_of<F(int)>::type;
+        auto func = std::move(m_func);
+        return Task<ReturnType(ArgsType...)>([func, &f](ArgsType&&... args)
+        {
+            std::future<int> ret = std::async(func, std::forward<ArgsType>(args)...);
+            return std::async(f, ret.get()).get();
+        });
+    }
+
+private:
+    std::function<R(ArgsType...)> m_func;
 };
 
 };
--- TaskCpp/main.cpp
+++ TaskCpp/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 #include "TaskCpp.hpp"
 
+void testTask()
+{
+    taskcpp::Task<void()> task([]{ std::cout << "task" << std::endl; });
+    task.wait();
+
+    taskcpp::Task<int()> task2([]{ std::cout << "task2" << std::endl; return 2; });
+    std::cout << "task2 ret: " << task2.get() << std::endl;
+
+    taskcpp::Task<int(int)> task3([](int i){ std::cout << "task3" << std::endl; return i; });
+    std::cout << "task3 ret: " << task3.get(3) << std::endl;
+}
+
 int main()
 {
+    testTask();
     return 0;
 }
 
