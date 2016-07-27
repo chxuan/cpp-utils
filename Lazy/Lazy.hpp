ab8a20547ec0c3a9f6fe3621d3054e09ee0ed31a
chxuan <787280310@qq.com>
2016-07-27 17:14:19 +0800

    Update lazy

--- Lazy/Lazy.hpp
+++ Lazy/Lazy.hpp
@@ -1,3 +1,35 @@
 #ifndef _LAZY_H
 #define _LAZY_H
+
+#include <iostream>
+#include <functional>
+#include <boost/optional.hpp>
+
+template<typename R>
+class Lazy
+{
+public:
+    template<typename Function, typename... Args>
+    Lazy(Function&& func, Args&&... args)
+    {
+        m_func = [&func, &args...]{ return func(std::forward<Args>(args)...); };
+    }
+
+    R& value()
+    {
+        m_value = m_func();
+        return *m_value;
+    }
+
+private:
+    std::function<R()> m_func;
+    boost::optional<R> m_value;
+};
+
+template<typename Function, typename... Args>
+Lazy<typename std::result_of<Function(Args...)>::type> lazy(Function&& func, Args&&... args)
+{
+    return Lazy<typename std::result_of<Function(Args...)>::type>(std::forward<Function>(func), std::forward<Args>(args)...);
+}
+
 #endif
--- Lazy/main.cpp
+++ Lazy/main.cpp
@@ -3,6 +3,12 @@
 
 int main()
 {
+    Lazy<int> lazyer = lazy([]{ return 10; });
+    std::cout << lazyer.value() << std::endl;
+
+    std::function<int(int)> f = [](int i){ return i; };
+    auto lazyer2 = lazy(f, 100);
+    std::cout << lazyer2.value() << std::endl;
     return 0;
 }
 
