e847700e6aa06784d99b868ab9f5e35361446199
chxuan <787280310@qq.com>
2016-07-29 16:16:13 +0800

    Update range

--- Range/main.cpp
+++ Range/main.cpp
@@ -3,6 +3,55 @@
 
 int main()
 {
+    std::cout << "range(5): ";
+    for (int i : range(5))
+    {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "range(2, 6): ";
+    for (int i : range(2, 6))
+    {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "range(10.5, 15.5): ";
+    for (int i : range(10.5, 15.5))
+    {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "range(35, 27, -1): ";
+    for (int i : range(35, 27, -1))
+    {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "range(2, 8, 0.5): ";
+    for (float i : range(2, 8, 0.5))
+    {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "range(8, 7, -0.1): ";
+    for (auto i : range(8, 7, -0.1))
+    {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "range('a', 'z'): ";
+    for (auto i : range('a', 'z'))
+    {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
     return 0;
 }
 
