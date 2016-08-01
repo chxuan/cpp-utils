a55d39eaddbac0fa5647f2e9e1982c787cf64e37
chxuan <787280310@qq.com>
2016-08-01 17:10:03 +0800

    Update tuplehelper

--- TupleHelper/main.cpp
+++ TupleHelper/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include "TuplePrinter.hpp"
+#include "TupleGet.hpp"
 
 int main()
 {
@@ -8,5 +9,12 @@ int main()
     std::tuple<int, short, double, char> tp = std::make_tuple(1, 2, 3, 'a');
     printTuple(tp);
     printTuple(std::forward_as_tuple("Hello", "world"));
+
+    std::cout << "******TupleGet*********" << std::endl;
+    auto a = getElementByType<double>(tp);
+    std::cout << "double: " << a << std::endl;
+    auto b = getElementByType<char>(tp);
+    std::cout << "char: " << b << std::endl;
+
     return 0;
 }
