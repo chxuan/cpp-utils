b9294863b554a58ab351520bff338a651e2837ff
chxuan <787280310@qq.com>
2016-08-08 15:26:46 +0800

    Update datetime

--- DateTime/DateTime.hpp
+++ DateTime/DateTime.hpp
@@ -26,14 +26,6 @@ public:
     }
 
 private:
-    static std::string time2string(time_t t)
-    {
-        struct tm* tmNow = localtime(&t);
-	char timeStr[sizeof("yyyy-mm-dd hh:mm:ss")] = {'\0'};
-	std::strftime(timeStr, sizeof(timeStr), "%Y-%m-%d %H:%M:%S", tmNow);
-	return timeStr;
-    }
-
     static time_t string2time(const std::string& timeStr)
     {
         struct tm stTm;
@@ -50,6 +42,14 @@ private:
         stTm.tm_isdst = -1;
 
         return mktime(&stTm);
+    }
+
+    static std::string time2string(time_t t)
+    {
+        struct tm* tmNow = localtime(&t);
+        char timeStr[sizeof("yyyy-mm-dd hh:mm:ss")] = {'\0'};
+        std::strftime(timeStr, sizeof(timeStr), "%Y-%m-%d %H:%M:%S", tmNow);
+        return timeStr;
     }
 };
 
