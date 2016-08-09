c9fa22aa24fbe5dfc95cdc5c53899cfb38187775
chxuan <787280310@qq.com>
2016-08-09 14:34:52 +0800

    Update string

--- String/String.hpp
+++ String/String.hpp
@@ -1,10 +1,65 @@
 #ifndef _STRING_H
 #define _STRING_H
 
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
 class String
 {
 public:
+    static std::string trimLeft(const std::string& str, const std::string& token = " ")
+    {
+        std::string t = str;
+        t.erase(0, t.find_first_not_of(token));
+        return t;
+    }
 
+    static std::string trimRight(const std::string& str, const std::string& token = " ")
+    {
+        std::string t = str;
+        t.erase(t.find_last_not_of(token) + 1);
+        return t;
+    }
+
+    static std::string trim(const std::string& str, const std::string& token = " ")
+    {
+        std::string t = str;
+        t.erase(0, t.find_first_not_of(token));
+        t.erase(t.find_last_not_of(token) + 1);
+        return t;
+    }
+
+    static std::string toLower(const std::string& str)
+    {
+        std::string t = str;
+        std::transform(t.begin(), t.end(), t.begin(), tolower);
+        return t;
+    }
+
+    static std::string toUpper(const std::string& str)
+    {
+        std::string t = str;
+        std::transform(t.begin(), t.end(), t.begin(), toupper);
+        return t;
+    }
+
+    static bool	startsWith(const std::string& str, const std::string& substr)
+    {
+        return str.find(substr) == 0;
+    }
+
+    static bool endsWith(const std::string& str, const std::string& substr)
+    {
+        return str.rfind(substr) == (str.length() - substr.length());
+    }
+
+    static bool equalsIgnoreCase(const std::string& str1, const std::string& str2) 
+    {
+        return toLower(str1) == toLower(str2);
+    }
 };
 
-#endif
+#endif 
+
--- String/main.cpp
+++ String/main.cpp
@@ -3,5 +3,15 @@
 
 int main()
 {
+    std::string str = "Hello world";
+    std::cout << String::trimLeft(str, "Hello") << std::endl;
+    std::cout << String::trimRight(str, "world") << std::endl;
+    str = "  nihao ";
+    std::cout << String::trim(str) << std::endl;
+    std::cout << String::toUpper(str) << std::endl;
+    std::cout << String::toLower(String::toUpper(str)) << std::endl;
+    str = "Hello world";
+    std::cout << String::startsWith(str, "Hello") << std::endl;
+    std::cout << String::endsWith(str, "a") << std::endl;
     return 0;
 }
