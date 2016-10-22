962cd03d47dcefdab8e5d7cd0852e27c5ca015bb
chxuan <787280310@qq.com>
2016-10-22 22:48:14 +0800

    update thread_pool

--- thread_pool/main.cpp
+++ thread_pool/main.cpp
@@ -1,12 +1,9 @@
-/************************************************
- * 该例程讲解用C++11来实现一个通用线程池
-************************************************/
 #include <iostream>
 #include <string>
 #include <chrono>
-#include "ThreadPool.hpp"
+#include "thread_pool.hpp"
 
-void testTask(const std::string& str)
+void test_task(const std::string& str)
 {
     std::cout << "Current thread id: " << std::this_thread::get_id() << ", str: " << str << std::endl;
     std::this_thread::sleep_for(std::chrono::milliseconds(50));
@@ -34,17 +31,17 @@ int main()
 {
     Test t;
     Test2 t2;
-    ThreadPool pool;
-    pool.initThreadNum(10);
+    thread_pool pool;
+    pool.init_thread_num(10);
 
     std::string str = "Hello world";
     
     for (int i = 0; i < 1000; ++i)
     {
-        pool.addTask([]{ std::cout << "Hello ThreadPool" << std::endl; });
-        pool.addTask(testTask, str);
-        pool.addTask(t2, str, i);
-        pool.addTask(&Test::print, &t, str, i);
+        pool.add_task([]{ std::cout << "Hello ThreadPool" << std::endl; });
+        pool.add_task(test_task, str);
+        pool.add_task(t2, str, i);
+        pool.add_task(&Test::print, &t, str, i);
     }
 
     std::cin.get();
---
+++ thread_pool/thread_pool.hpp
@@ -0,0 +1,167 @@
+#ifndef _THREADPOOL_H
+#define _THREADPOOL_H
+
+#include <vector>
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <memory>
+#include <functional>
+#include <condition_variable>
+#include <atomic>
+#include <type_traits>
+
+static const std::size_t max_task_quque_size = 100000;
+static const std::size_t max_thread_size = 30;
+
+class thread_pool
+{
+public:
+    using work_thread_ptr = std::shared_ptr<std::thread>;
+    using task_t = std::function<void()>; 
+
+    explicit thread_pool() : _is_stop_threadpool(false) {}
+
+    ~thread_pool()
+    {
+        stop();
+    }
+
+    void init_thread_num(std::size_t num)
+    {
+        if (num <= 0 || num > max_thread_size)
+        {
+            std::string str = "Number of threads in the range of 1 to " + std::to_string(max_thread_size);
+            throw std::invalid_argument(str);
+        }
+
+        for (std::size_t i = 0; i < num; ++i)
+        {
+            work_thread_ptr t = std::make_shared<std::thread>(std::bind(&thread_pool::run_task, this));
+            _thread_vec.emplace_back(t);
+        }
+    }
+
+    template<typename Function, typename... Args>
+    void add_task(const Function& func, Args... args)
+    {
+        if (!_is_stop_threadpool)
+        {
+            task_t task = [&func, args...]{ return func(args...); };
+            add_task_impl(task);
+        }
+    }
+
+    template<typename Function, typename... Args>
+    typename std::enable_if<std::is_class<Function>::value>::type add_task(Function& func, Args... args)
+    {
+        if (!_is_stop_threadpool)
+        {
+            task_t task = [&func, args...]{ return func(args...); };
+            add_task_impl(task);
+        }
+    }
+
+    template<typename Function, typename Self, typename... Args>
+    void add_task(const Function& func, Self* self, Args... args)
+    {
+        if (!_is_stop_threadpool)
+        {
+            task_t task = [&func, &self, args...]{ return (*self.*func)(args...); };
+            add_task_impl(task);
+        }
+    }
+
+    void stop()
+    {
+        std::call_once(_call_flag, [this]{ terminate_all(); });
+    }
+
+private:
+    void add_task_impl(const task_t& task)
+    {
+        {
+            std::unique_lock<std::mutex> locker(_task_queue_mutex);
+            while (_task_queue.size() == max_task_quque_size && !_is_stop_threadpool)
+            {
+                _task_put.wait(locker);
+            }
+
+            _task_queue.emplace(std::move(task));
+        }
+
+        _task_get.notify_one();
+    }
+
+    void terminate_all()
+    {
+        _is_stop_threadpool = true;
+        _task_get.notify_all();
+
+        for (auto& iter : _thread_vec)
+        {
+            if (iter != nullptr)
+            {
+                if (iter->joinable())
+                {
+                    iter->join();
+                }
+            }
+        }
+        _thread_vec.clear();
+
+        clean_task_queue();
+    }
+
+    void run_task()
+    {
+        while (true)
+        {
+            task_t task = nullptr;
+            {
+                std::unique_lock<std::mutex> locker(_task_queue_mutex);
+                while (_task_queue.empty() && !_is_stop_threadpool)
+                {
+                    _task_get.wait(locker);
+                }
+
+                if (_is_stop_threadpool)
+                {
+                    break;
+                }
+
+                if (!_task_queue.empty())
+                {
+                    task = std::move(_task_queue.front());
+                    _task_queue.pop();
+                }
+            }
+
+            if (task != nullptr)
+            {
+                task();
+                _task_put.notify_one();
+            }
+        }
+    }
+
+    void clean_task_queue()
+    {
+        std::lock_guard<std::mutex> locker(_task_queue_mutex);
+        while (!_task_queue.empty())
+        {
+            _task_queue.pop();
+        }
+    }
+
+private:
+    std::vector<work_thread_ptr> _thread_vec;
+    std::condition_variable _task_put;
+    std::condition_variable _task_get;
+    std::mutex _task_queue_mutex;
+    std::queue<task_t> _task_queue;
+    std::atomic<bool> _is_stop_threadpool;
+    std::once_flag _call_flag;
+};
+
+#endif
