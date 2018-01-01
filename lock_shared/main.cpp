616b164dc20fdd8b017c7a60b68f9963bb96e496
chxuan <787280310@qq.com>
2018-01-01 11:08:22 +0800

    update lock_shared

--- lock_shared/main.cpp
+++ lock_shared/main.cpp
@@ -1,6 +1,52 @@
 #include "lock_shared.h"
-
-int main()
+#include <iostream>
+#include <thread>
+ 
+class ThreadSafeCounter 
 {
+public:
+    ThreadSafeCounter() = default;
+
+    unsigned int get() 
+    {
+        lock_shared lock(mutex_, true);
+        return value_;
+    }
+
+    void increment() 
+    {
+        lock_shared lock(mutex_);
+        value_++;
+    }
+
+    void reset() 
+    {
+        lock_shared lock(mutex_);
+        value_ = 0;
+    }
+
+private:
+    shared_mutex mutex_;
+    unsigned int value_ = 0;
+};
+
+int main() 
+{
+    ThreadSafeCounter counter;
+
+    auto increment_and_print = [&counter]() 
+    {
+        for (int i = 0; i < 3; i++) {
+            counter.increment();
+            std::cout << std::this_thread::get_id() << ' ' << counter.get() << '\n';
+        }
+    };
+
+    std::thread thread1(increment_and_print);
+    std::thread thread2(increment_and_print);
+
+    thread1.join();
+    thread2.join();
+
     return 0;
 }
