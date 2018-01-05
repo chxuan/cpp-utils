0dc842055ac71f54f274b1a6cacb5620cb2e5e19
chxuan <787280310@qq.com>
2018-01-05 21:05:33 +0800

    update random generator

--- random_generator/main.cpp
+++ random_generator/main.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
+#include "random_generator.h"
+
+int main()
+{
+    for (int i = 1; i < 10; ++i)
+    {
+        random_generator random;
+        std::cout << random.randint(1, 10) << std::endl; 
+    }
+
+    std::cout << "---------------" << std::endl;
+    for (int i = 1; i < 10; ++i)
+    {
+        random_generator random;
+        std::cout << random.randreal(1, 2) << std::endl;
+    }
+
+    return 0;
+}
 
--- random_generator/random_generator.h
+++ random_generator/random_generator.h
@@ -1,24 +1,37 @@
+/**
+ * @file random_generator.h
+ * @brief 随机数生成器
+ * @author chxuan, 787280310@qq.com
+ * @version 1.0.0
+ * @date 2018-01-05
+ */
 #pragma once
 
 #include <random>
 #include <chrono>
-using namespace std;
 
 class random_generator
 {
 public:
     random_generator() 
     { 
-        engine.seed(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+        engine_.seed(std::chrono::high_resolution_clock::now().time_since_epoch().count());
     }
 
     // 生成int类型随机数，返回范围[min, max]
-    int randint(int min,int max)
+    int randint(int min, int max)
     {
-        uniform_int_distribution<int> u(min,max);
-        return u(engine);
+        std::uniform_int_distribution<int> u(min, max);
+        return u(engine_);
+    }
+
+    // 生成double类型随机数，返回范围[min, max]
+    double randreal(double min, double max)
+    {
+        std::uniform_real_distribution<double> u(min, max);
+        return u(engine_);
     }
 
 private:
-    std::default_random_engine engine;
+    std::default_random_engine engine_;
 };
